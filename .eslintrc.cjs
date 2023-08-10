cb7b6c343969bbae7d5174f4ad1f05c2ae9157b5
Herway <hqw6@qq.com>
2023-08-10 14:09:58 +0800

    feat: :sparkles: 部署项目基本结构

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,17 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 'latest',
   },
   rules: {
-    "no-undef": "off",
-  }
+    'no-undef': 'off',
+    'vue/comment-directive': 'off',
+  },
 }
